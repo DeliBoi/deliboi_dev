99ff32fde736528b797877e1b827999af5a95aeb
mwachala <berrybretch@gmail.com>
2020-08-01 20:46:11 +0300

    Ui Component first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Text } from 'react-native'
+import SignInScreen from './Components/Sign_in'
+
+
+const App = () => {
+    return (
+        <SignInScreen></SignInScreen>
+    )
+}
+
+export default App;
+
